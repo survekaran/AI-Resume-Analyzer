dec69e1d2fcedaebbfc94431153880c84e14f105
survekaran <survekaran71@gmail.com>
2025-12-13 01:50:31 +0530

    Updated homepage UI

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,35 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "~/constants";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    return [
+        { title: "Resumifier" },
+        { name: "description", content: "AI Resume Analyzer and application tracking tool." },
+    ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    return (
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen">
+            <Navbar />
+
+            <section className="main-section">
+                <div className="page-heading">
+                    <h1>Track your application & Resume Rating</h1>
+                    <h2>Review your submission and check AI-powered feedback.</h2>
+                </div>
+            </section>
+
+            {resumes.length > 0 && (
+                <div className="resumes-section">
+                    {resumes.map((resume) => (
+                        <ResumeCard key={resume.id} resume={resume} />
+                    ))}
+            </div>
+
+            )}
+        </main>
+    );
 }
